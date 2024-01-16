ed62c8d4532eb6116b26a4a89777525920640837
Keelin999 <keelcoll@flemingcollege.ca>
2024-01-16 08:23:29 -0500

    Created script files
    Added code that prints lines from Zen of Python.

---
+++ script1.py
@@ -0,0 +1,2 @@
+print(f"Beautiful is better then ugly.")
+print(f"Explicit is better than implicit.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print(f"Errors should never pass silently.")
+print(f"Unless explicitly silenced.")
