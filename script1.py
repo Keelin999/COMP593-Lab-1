32aeffffcc43614c1bf19468ba3fbb10fc4fd4d8
Keelin999 <keelcoll@flemingcollege.ca>
2024-01-16 08:32:18 -0500

    Revert "Modified both script files"
    This reverts commit cf589d39e6a262f7c7e58c506910a625cfc22a95.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 #Print lines from the Zen of Python
 print(f"Beautiful is better then ugly.")
+print(f"Explicit is better than implicit.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-#Print lines from the Zen of Python
 print(f"Errors should never pass silently.")
 print(f"Unless explicitly silenced.")
