56db9318ba50927a55caa9dd47d3cadc2cb5bf98
Keelin999 <keelcoll@flemingcollege.ca>
2024-01-16 08:43:13 -0500

    Modified both files in repo clone
    -Deleted print statement from script1.py
    -Added print statement from script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #Print lines from the Zen of Python
 print(f"Beautiful is better then ugly.")
-print(f"Explicit is better than implicit.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #Print lines from the Zen of Python
 print(f"Errors should never pass silently.")
 print(f"Unless explicitly silenced.")
+print(f"Unless explicitly silenced.")
